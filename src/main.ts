e1cb17eab80ab61e4bc2b3fbc9f0e79bd877efc0
DavidKal29 <davidkalmuk65@gmail.com>
2025-11-06 11:06:28 +0100

    Añadimos dotenv, y cookie parser, para leer variables de entorno y cookies. Habilitamos CORS para que haya comunicación entre backend y frontend, y creamos el servicio, controlador y module para las rutas de los productos de la tienda

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from'cookie-parser'
+import dotenv from 'dotenv'
+dotenv.config()
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(new ValidationPipe())
+
+  app.use(cookieParser())
+
+  app.enableCors({
+    origin:process.env.FRONTEND_URL,
+    credentials: true
+  })
+
+
+  await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
