76649df4157c05fba2224aace0a9ffa24ba88543
DavidKal29 <davidkalmuk65@gmail.com>
2025-11-06 16:15:27 +0100

    Cambiamos la ruta de getProducts, a getNewProducts, la cual devuelve los productos más recientes, mientras que getProducts ahora debe tener un parámetro, el cual es la categoria para obtener los productos

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,13 +1,18 @@
-import { Controller,Get } from '@nestjs/common';
+import { Controller,Get, Param } from '@nestjs/common';
 import { ProductsService } from './products.service';
 
 @Controller('products')
 export class ProductsController {
     constructor (private readonly productsService:ProductsService){}
 
-    @Get('getProducts')
-    getProducts(){
-        return this.productsService.getProducts()
+    @Get('getProducts/:category')
+    getProducts(@Param('category') category:string){
+        return this.productsService.getProducts(category)
+    }
+
+    @Get('getNewProducts')
+    getNewProducts(){
+        return this.productsService.getNewProducts()
     }
 
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -3,12 +3,30 @@ import { conectarDB } from '../../src/database/mongo.js'
 
 @Injectable()
 export class ProductsService {
-    async getProducts(){
+    async getProducts(category:String){
         try {
             const db = await conectarDB()
             const products = db.collection('products')
 
-            const dataProducts = await products.find({}).toArray()
+            const dataProducts = await products.find({category:category}).toArray()
+
+            console.log('Los productos:',dataProducts);
+
+            return {products: dataProducts}
+        } catch (error) {
+            console.log(error);
+
+            return {error:'Error al obtener los productos'}
+            
+        }
+    }
+
+    async getNewProducts(){
+        try {
+            const db = await conectarDB()
+            const products = db.collection('products')
+
+            const dataProducts = await products.find().limit(10).toArray()
 
             console.log('Los productos:',dataProducts);
 
