1d69de07baa64781037745bb8e3ca2afe8a1c10f
DavidKal29 <davidkalmuk65@gmail.com>
2025-11-06 11:21:36 +0100

    Añadimos la ruta getProducts, en la carpeta de products, para poder obtener todos los productos de la tienda. Además, ahora el proyecto se llama Neutrino-Ultra y no Clash-Fist

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { conectarDB } from '../../src/database/mongo.js'
 
 @Injectable()
-export class ProductsService {}
+export class ProductsService {
+    async getProducts(){
+        try {
+            const db = await conectarDB()
+            const products = db.collection('products')
+
+            const dataProducts = await products.find({}).toArray()
+
+            console.log('Los productos:',dataProducts);
+
+            return {products: dataProducts}
+        } catch (error) {
+            console.log(error);
+
+            return {error:'Error al obtener los productos'}
+            
+        }
+    }
+}
